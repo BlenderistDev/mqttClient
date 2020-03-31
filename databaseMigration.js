9067f23da8a47ce4a113dd7a89d7f2ced09b0c0c
blenderist <demonx.blenderist@gmail.com>
2020-03-31 18:03:57 +0300

    Актуализировал миграцию базы данных

--- databaseMigration.js
+++ databaseMigration.js
@@ -1,9 +1,11 @@
-const ShellSubscriptions = require('./modules/shell/model/ShellSubscriptions').sync;
-const Subscriptions = require('./handlers/model/Subscriptions').sync;
-const Senders = require('./handlers/model/Senders').sync;
-const ShellSenders = require('./modules/shell/model/ShellSenders').sync;
-const MqttMessages = require('./modules/mqttLogger/model/mqttMessages').sync;
-const MessagesCounter = require('./modules/messageCounter/model/MessagesCounter').sync;
+require('./config/constants');
+
+const ShellSubscriptions = require('./src/Modules/Shell/model/ShellSubscriptions').sync;
+const Subscriptions = require('./src/Modules/Main/model/Subscriptions').sync;
+const Senders = require('./src/Modules/Main/model/Senders').sync;
+const ShellSenders = require('./src/Modules/Shell/model/ShellSenders').sync;
+const MqttMessages = require('./src/Modules/MqttLogger/model/mqttMessages').sync;
+const MessagesCounter = require('./src/Modules/MessageCounter/model/MessagesCounter').sync;
 
 ShellSubscriptions.sync();
 Subscriptions.sync();
