a7c10574aeccd04ed76344874a130a6ce8ccbf1d
blenderist <demonx.blenderist@gmail.com>
2021-03-10 21:45:53 +0300

    Продолжение работы над фронтом

--- front/src/components/ModuleSettings.vue
+++ front/src/components/ModuleSettings.vue
@@ -5,9 +5,15 @@ div {{ moduleName }}
 <script>
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
+import Form from 'Form'
+import List from 'List'
 
 export default {
   name: 'ModuleList',
+  components: {
+    Form,
+    List
+  },
   props: ['name'],
   setup () {
     const router = useRoute()
