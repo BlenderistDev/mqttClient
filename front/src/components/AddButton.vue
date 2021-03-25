9def3bef298c7c3d21dff0ada2a8c7db31ae920d
blenderist <demonx.blenderist@gmail.com>
2021-03-25 13:12:53 +0300

    Докомит. Компонент кнопи добавления конфига

---
+++ front/src/components/AddButton.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+div
+  button(@click="addConfig") Добавить
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  methods: {
+    add() {},
+    ...mapActions(["addConfig"]),
+  },
+};
+</script>
