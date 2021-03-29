bc19e0b47a8fdb2349722c8bb8d1efb393e8a4c6
blenderist <demonx.blenderist@gmail.com>
2021-03-29 21:25:21 +0300

    Удалил недосягаемый код

--- src/Modules/Main/Api.js
+++ src/Modules/Main/Api.js
@@ -6,7 +6,6 @@ import _ from 'lodash'
 export class Api extends ApiPrototype {
   cmdModuleList () {
     return getManager().aModules
-    return _.filter(getManager().aModules, module => !_.isUndefined(module.api))
   }
 }
 
