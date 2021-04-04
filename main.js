cbd347bb593c30f76dd1caa7b8fb73fcd0c8ba59
blenderist <demonx.blenderist@gmail.com>
2021-04-04 17:54:48 +0300

    Удалил константы окружения. Заменил их ипользование на обычные пути

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-import { setConstants } from './config/constants.js';
 import { getManager } from './src/core/index.js';
 import {app} from './src/core/index.js';
 
-setConstants();
 getManager().start()
 
 const api = app;
