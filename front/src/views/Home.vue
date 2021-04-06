ba43f8cd3b8f6aa477dbceaf95cfadbc5a36a9ef
BlenderistDev <demonx.blenderist@gmail.com>
2021-04-06 13:14:21 +0300

    Рефакторинг фронта

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,15 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+.home
+  ModuleList
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ModuleList from '@/components/ModuleList'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    ModuleList
   }
 }
 </script>
