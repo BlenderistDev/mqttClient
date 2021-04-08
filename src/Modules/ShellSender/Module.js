fcffe1d2cb49f7f60bbd528a4e244edbb18ad636
blenderist <demonx.blenderist@gmail.com>
2021-04-08 20:12:54 +0300

    Перевел модуль отправителя командной строки на отдельный процесс

--- src/Modules/ShellSender/Module.js
+++ src/Modules/ShellSender/Module.js
@@ -1,24 +1,13 @@
 import shell from 'shelljs';
-import {ModulePrototype} from '../../core/ModuleManager/ModulePrototype.js';
 import {sendDiscoveryMessage} from '../../core/HomeAssistant/HomeAssistant.js';
+import {mqttClient} from '../../core/Mqtt/MqttClient.js';
 import _ from 'lodash';
+import md5 from 'md5'
 
-/**
- * Класс для модуля командной строки
- */
-export class Module extends ModulePrototype {
-  /**
-   * Конструктор модуля командной строки
-   */
-  constructor(moduleTopic, config) {
-    super(moduleTopic, config);
-    _.map(this.config, sender => {
-      const topic = `${this.getTopic()}/${sender.topic}`
-      sendDiscoveryMessage(topic, topic, 'sensor', {});
-      setInterval(() => {
-        const oCommandLineSensorData = shell.exec(sender.command, {'silent': true})
-        this.sendMessage(oCommandLineSensorData.toString(), topic)
-      }, sender.interval);
-    })
-  }
-}
+const config = JSON.parse(process.argv[2]);
+
+_.map(config.config, sender => {
+  const topic = `${config.topic}/${sender.topic}`
+  sendDiscoveryMessage(md5(topic), topic, 'sensor', {});
+  setInterval(() => mqttClient.sendMessage(topic, shell.exec(sender.command, {'silent': true}).toString()), sender.interval);
+})
