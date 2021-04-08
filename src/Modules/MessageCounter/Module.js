3f97e1443a97aa2b16b219848e64515ea1cd8427
blenderist <demonx.blenderist@gmail.com>
2021-04-08 19:47:26 +0300

    Исправил работу счетчика сообщений после перевода на отдельный процесс

--- src/Modules/MessageCounter/Module.js
+++ src/Modules/MessageCounter/Module.js
@@ -2,14 +2,16 @@ import {Counter} from './Counter.js';
 import _ from 'lodash'
 
 const config = JSON.parse(process.argv[2]);
-const aCounters = _.map(config.value, row => {
+const counters = [];
+
+_.map(config.config, row => {
   const counterName = `messages_in_${row.interval}_sec`;
   const counterTopic = `${config.topic}/${counterName}`;
-  return new Counter(counterTopic, row.interval, counterName);
+  counters.push(new Counter(counterTopic, row.interval, counterName));
 })
 
 process.on('message', (mqttMessage) => {
-  aCounters.forEach((oCounter) => {
+  counters.forEach((oCounter) => {
     oCounter.increase();
   });
 });
