4efa55d8de0c050179234c8389dd57e7dd29a395
BlenderistDev <demonx.blenderist@gmail.com>
2021-04-08 16:56:21 +0300

    Перевел модуль счетчика сообщений на процессы

--- src/Modules/MessageCounter/Module.js
+++ src/Modules/MessageCounter/Module.js
@@ -1,33 +1,15 @@
 import {Counter} from './Counter.js';
-import {ModulePrototype} from '../../core/ModuleManager/ModulePrototype.js';
 import _ from 'lodash'
 
-/**
- * Модуль для счетчиков сообщений за период
- */
-export class Module extends ModulePrototype {
-  /**
-   * @param {string} moduleTopic
-   * Создаем массив счетчиков
-   */
-  constructor(moduleTopic, config) {
-    super(moduleTopic, config);
-    this.aCounters = _.map(this.config, row => {
-      const counterName = `messages_in_${row.interval}_sec`;
-      const counterTopic = `${this.getTopic()}/${counterName}`;
-      return new Counter(counterTopic, row.interval, counterName);
-    })
-  }
+const config = JSON.parse(process.argv[2]);
+const aCounters = _.map(config.value, row => {
+  const counterName = `messages_in_${row.interval}_sec`;
+  const counterTopic = `${config.topic}/${counterName}`;
+  return new Counter(counterTopic, row.interval, counterName);
+})
 
-  /**
-   * Обработчик сообщения
-   * @param {string} sTopic
-   * @param {string} sMessage
-   */
-  handleMessage(sTopic, sMessage) {
-    this.aCounters.forEach((oCounter) => {
-      oCounter.increase();
-    });
-  }
-}
-
+process.on('message', (mqttMessage) => {
+  aCounters.forEach((oCounter) => {
+    oCounter.increase();
+  });
+});
