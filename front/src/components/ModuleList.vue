3e3d383ab67592dc21af16cc9b6aceaa2dfe2c41
blenderist <demonx.blenderist@gmail.com>
2021-04-08 19:54:59 +0300

    Доработки под новый формат списка модулей

--- front/src/components/ModuleList.vue
+++ front/src/components/ModuleList.vue
@@ -1,28 +1,27 @@
-
 <template lang="pug">|
 div
   router-link(
     v-for="module in moduleList"
-    :to="{ name: 'module', params: { name: module.name } }"
+    :to="{ name: 'module', params: { name: module } }"
   ) 
-    div {{ module.name }}
+    div {{ module }}
 </template>
 
 <script>
-import { computed } from 'vue'
-import { useStore } from 'vuex'
+import { computed } from "vue";
+import { useStore } from "vuex";
 
 export default {
-  name: 'ModuleSettings',
+  name: "ModuleSettings",
   props: {
-    msg: String
+    msg: String,
   },
-  setup () {
-    const store = useStore()
-    store.dispatch('fetchModuleList')
+  setup() {
+    const store = useStore();
+    store.dispatch("fetchModuleList");
     return {
-      moduleList: computed(() => store.state.moduleList)
-    }
-  }
-}
+      moduleList: computed(() => store.state.moduleList),
+    };
+  },
+};
 </script>
