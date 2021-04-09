f5073a5d455193d4dc14b7deea4f1b849a05aa83
BlenderistDev <demonx.blenderist@gmail.com>
2021-04-09 13:09:55 +0300

    перевел модуль дптчика движения на отдельный процесс

--- src/Modules/MotionSensor/Module.js
+++ src/Modules/MotionSensor/Module.js
@@ -1,52 +1,14 @@
 import _ from 'lodash';
-import {ModulePrototype} from '../../core/ModuleManager/ModulePrototype.js';
 import {MotionSensor} from './MotionSensor.js';
 
-/**
- * Класс для модуля датчика движения
- */
-export class Module extends ModulePrototype {
-  /**
-   * @param {string} moduleTopic
-   * Конструктор модуля датчика движения
-   */
-  constructor(moduleTopic, config) {
-    super(moduleTopic, config);
-    this.motionSensors = [];
-    this.topicList = [];
-    this.setMotionSensors();
-  }
+const config = JSON.parse(process.argv[2])
 
-  /**
-   * Инициализируем датчики
-   */
-  async setMotionSensors() {
-    _.map(this.config, sensor => {
-      this.motionSensors.push(new MotionSensor(sensor, this.getTopic()));
-      this.topicList.push(sensor.in_topic);
-    })
-  }
+const motionSensors = _.map(config.config, sensor => new MotionSensor(sensor, config.topic))
 
-
-  /**
-   * Передаем значение нужному датчику
-   * @param {string} sTopic
-   * @param {string} sMessage
-   */
-  handleMessage(sTopic, sMessage) {
-    this.motionSensors.forEach((sensor) => {
-      if (sTopic === sensor.inSensor.topic) {
-        sensor.handleMessage(sMessage);
-      }
-    });
-  }
-
-  /**
-   * Модуль подписан на входящие топики датчиков
-   * @param {string} sTopic
-   * @return {bool}
-   */
-  isTopicInSubscription(sTopic) {
-    return (this.topicList.indexOf(sTopic) >= 0);
-  }
-}
+process.on('message', (mqttMessage) => {
+  motionSensors.forEach((sensor) => {
+    if (mqttMessage.topic === sensor.inSensor.topic) {
+      sensor.handleMessage(mqttMessage.message)
+    }
+  })
+})
