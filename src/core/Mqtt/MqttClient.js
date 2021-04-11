d913e264acef559d409cb78daea36873f9899878
blenderist <demonx.blenderist@gmail.com>
2021-04-11 09:49:56 +0300

    Рефакторинг

--- src/core/Mqtt/MqttClient.js
+++ src/core/Mqtt/MqttClient.js
@@ -3,20 +3,14 @@ import EventEmitter from 'events';
 import { getConfig } from '../Config/Config.js'
 
 const config = getConfig('mqtt')
-
-export const mqttPrefix = config.topic
-
 const mqttClient = new EventEmitter()
-
 const connection = mqtt.connect(config.host, {
   username: config.username,
   password: config.password
 })
-
 connection.on('connect', () => connection.subscribe('#'))
 connection.on('message', (topic, message) => mqttClient.emit('message', topic.toString(), message.toString()))
-
-mqttClient.sendMessage = (topic, message, retain = false) => connection.publish(topic.toString(), message.toString(), {retain: retain})
+mqttClient.sendMessage = (topic, message, retain = false) => connection.publish(topic.toString(), message.toString(), { retain: retain })
 
 export {mqttClient}
-
+export const mqttPrefix = config.topic
