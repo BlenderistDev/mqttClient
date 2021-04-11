3158c8a1e506a226b938edcfa038f50a2ab2f3c5
blenderist <demonx.blenderist@gmail.com>
2021-04-11 10:06:14 +0300

    Рефакторинг логгера

--- src/core/Logger.js
+++ src/core/Logger.js
@@ -1,16 +1,14 @@
-import winston from 'winston';
-const {createLogger, format, transports} = winston;
-const {combine, timestamp, printf} = format;
-const myFormat = printf(({message, timestamp}) => {
-  return `${timestamp}: ${message}`;
-});
+import winston from 'winston'
+
+const { createLogger, format, transports}  = winston
+const { combine, timestamp, printf } = format
+const myFormat = printf(({ message, timestamp }) => `${timestamp}: ${message}`)
 
 const logger = createLogger({
   format: combine(timestamp(), myFormat),
   transports: [
-    new transports.Console(),
-    new transports.File({filename: 'log/error.log'}),
+    new transports.File({ filename: 'log/error.log' }),
   ],
 });
 
-export {logger};
+export { logger }
