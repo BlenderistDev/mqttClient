86bdd353668eb9727903ab453884b5954b9a857e
blenderist <demonx.blenderist@gmail.com>
2021-04-15 13:43:07 +0300

    Работа над новым форматом вывода сообщений

--- front/src/components/Socket.vue
+++ front/src/components/Socket.vue
@@ -1,22 +1,45 @@
 <template lang="pug">
 div
-  .card(v-for="message in messages")
+  div all
+    input(type="radio" v-model="currentView" value="all")
+  div topic
+    input(type="radio" v-model="currentView" value="topic")
+
+  .card(v-if="currentView === 'all'" v-for="message in messages")
     .card-header {{ message.topic }}
     .card-body {{ message.message }}
+  
+  .card(v-if="currentView === 'topic'" v-for="(messages, topic) in groupedMessages")
+    .card-header {{ topic }}
 </template>
 
 <script>
 import openSocket from "socket.io-client";
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import _ from "lodash";
 
 export default {
   setup() {
     const messages = ref([]);
+    const currentView = ref("all");
     const socket = openSocket("http://localhost:3000", { transports: ["websocket"] });
     socket.on("mqtt", (message) => {
       messages.value.unshift(message);
     });
-    return { messages };
+    const groupedMessages = computed(() =>
+      _.reduce(
+        messages.value,
+        (groupedMessages, message) => {
+          if (_.isUndefined(groupedMessages[message.topic])) {
+            groupedMessages[message.topic] = [];
+          }
+          groupedMessages[message.topic].push(message.message);
+          return groupedMessages;
+        },
+        {}
+      )
+    );
+    return { messages, groupedMessages, currentView };
   },
 };
 </script>
