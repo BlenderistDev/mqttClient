a617bb952eb6ddca501a463eb78d2a9e5efb0614
blenderist <demonx.blenderist@gmail.com>
2021-04-17 15:41:44 +0300

    Добавил фильтрацию по топику для списка всех сообщений

--- front/src/components/Messages/AllMessages.vue
+++ front/src/components/Messages/AllMessages.vue
@@ -1,11 +1,38 @@
 <template lang="pug">
-.card(v-for="message in messages")
-  .card-header {{ message.topic }}
-  .card-body {{ message.message }}  {{ message.date }}
+div
+  .row
+    .col-md-1 Topic:
+    .col-11
+      input(v-model='filterTopic')
+  .card(v-for="message in filtredMessages")
+    .card-header {{ message.topic }}
+    .card-body {{ message.message }}  {{ message.date }}
 </template>
 
 <script>
+import { ref, computed } from "vue";
+import _ from "lodash";
+
 export default {
   props: ["messages"],
+  setup(props) {
+    const filterTopic = ref("");
+    const filtredMessages = computed(() =>
+      _.filter(
+        props.messages,
+        (message) => !filterTopic.value || message.topic.includes(filterTopic.value)
+      )
+    );
+    return {
+      filterTopic,
+      filtredMessages,
+    };
+  },
 };
 </script>
+
+<style scoped>
+input {
+  width: 100%;
+}
+</style>
