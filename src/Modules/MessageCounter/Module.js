89942eb9984a16804c49cef0da20ed4ca6660675
blenderist <demonx.blenderist@gmail.com>
2021-04-19 20:08:22 +0300

    Рефакторинг счетчика сообщений

--- src/Modules/MessageCounter/Module.js
+++ src/Modules/MessageCounter/Module.js
@@ -1,17 +1,18 @@
-import {Counter} from './Counter.js';
+import { mqttClient } from '../../core/MqttClient.js'
+import { sendDiscoveryMessage } from '../../core/HomeAssistant.js'
 import _ from 'lodash'
 
 const config = JSON.parse(process.argv[2]);
-const counters = [];
 
 _.map(config.config, row => {
-  const counterName = `messages_in_${row.interval}_sec`;
-  const counterTopic = `${config.topic}/${counterName}`;
-  counters.push(new Counter(counterTopic, row.interval, counterName));
-})
+  const name = `messages_in_${row.interval}_sec`
+  const topic = `${config.topic}/${name}`
+  sendDiscoveryMessage(name, topic, 'sensor', { unit_of_measurement: 'messages' })
 
-process.on('message', (mqttMessage) => {
-  counters.forEach((oCounter) => {
-    oCounter.increase();
-  });
-});
+  let count = 0
+  process.on('message', message => count++)
+  setInterval(() => {
+    mqttClient.sendMessage(topic, count.toString())
+    count = 0;
+  }, row.interval * 1000)
+})
