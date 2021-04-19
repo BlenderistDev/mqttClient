3b8ff687832400f2fe141b6acf6246f7f35ffb3f
blenderist <demonx.blenderist@gmail.com>
2021-04-19 19:53:21 +0300

    На фронте удалил неиспользуемый компонент. Переименовал компонент Socket

--- front/src/components/Messages/Messages.vue
+++ front/src/components/Messages/Messages.vue
@@ -1,9 +1,59 @@
 <template lang="pug">
-div(v-for="message in messages") {{ message.message }} {{ message.date }}
+div
+  ul(class="nav nav-tabs nav-fill")
+    li(class="nav-item")
+      a(:class="getTabClass('all')" @click="setActiveTab('all')") All
+    li(class="nav-item")
+      a(:class="getTabClass('topic')" @click="setActiveTab('topic')") Topic
+
+  AllMessages(
+    v-if="currentView === 'all'"
+    :messages="messages"
+  )
+  TopicMessages(
+    v-if="currentView === 'topic'" 
+    :messages="groupedMessages"
+  )
 </template>
 
 <script>
+import openSocket from "socket.io-client";
+import { ref } from "vue";
+import _ from "lodash";
+import TopicMessages from "./TopicMessages";
+import AllMessages from "./AllMessages";
+
 export default {
-  props: ["messages"],
+  components: {
+    TopicMessages,
+    AllMessages,
+  },
+  setup() {
+    const messages = ref([]);
+    const groupedMessages = ref({});
+    const currentView = ref("all");
+
+    const socket = openSocket("http://localhost:3000", { transports: ["websocket"] });
+    socket.on("mqtt", (message) => {
+      messages.value.unshift(message);
+      if (_.isUndefined(groupedMessages.value[message.topic])) {
+        groupedMessages.value[message.topic] = [];
+      }
+      groupedMessages.value[message.topic].unshift(message);
+    });
+    return { messages, groupedMessages, currentView };
+  },
+  methods: {
+    setActiveTab(tab) {
+      this.currentView = tab;
+    },
+    getTabClass(tab) {
+      let tabClass = "nav-link";
+      if (tab === this.currentView) {
+        tabClass += " active";
+      }
+      return tabClass;
+    },
+  },
 };
 </script>
