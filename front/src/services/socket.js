afe2adac36f9a21d2c437134c7ec761bb60185ad
blenderist <demonx.blenderist@gmail.com>
2021-04-21 19:54:04 +0300

    Вынес адрес сокета в переменные окружения

--- front/src/services/socket.js
+++ front/src/services/socket.js
@@ -1,10 +1,12 @@
-import { ref } from "vue";
-import openSocket from "socket.io-client";
+import { ref } from "vue"
+import openSocket from "socket.io-client"
 
-const socket = openSocket("http://localhost:3000", { transports: ["websocket"] });
+const VUE_APP_SOCKET_URL = process.env.VUE_APP_SOCKET_URL
+
+const socket = openSocket(VUE_APP_SOCKET_URL, { transports: ["websocket"] })
 
 export function getSocket(topic) {
-  const data = ref('');
-  socket.on(topic, message => data.value = message);
+  const data = ref('')
+  socket.on(topic, message => data.value = message)
   return data
 }
