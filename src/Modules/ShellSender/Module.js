840e42c4a7d4968d4f36fe2e2ad13c3c6e361db0
blenderist <demonx.blenderist@gmail.com>
2021-04-28 19:41:16 +0300

    Исправил ошибку при пустой команде для модуля ShellSender

--- src/Modules/ShellSender/Module.js
+++ src/Modules/ShellSender/Module.js
@@ -9,5 +9,7 @@ const config = JSON.parse(process.argv[2]);
 _.map(config.config, sender => {
   const topic = `${config.topic}/${sender.topic}`
   sendDiscoveryMessage(md5(topic), topic, 'sensor', {});
-  setInterval(() => mqttClient.sendMessage(topic, shell.exec(sender.command, {'silent': true}).toString()), sender.interval);
+  if (sender.command) {
+    setInterval(() => mqttClient.sendMessage(topic, shell.exec(sender.command, {'silent': true}).toString()), sender.interval);
+  }
 })
