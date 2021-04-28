0c6e97aaba44c0d94924ddc243eb74eafdf308ef
BlenderistDev <demonx.blenderist@gmail.com>
2021-04-28 14:29:35 +0300

    Настроил интерфейс модуля MotionSensor

--- src/Modules/MotionSensor/config.js
+++ src/Modules/MotionSensor/config.js
@@ -4,35 +4,43 @@ export default {
   fields: {
     in_topic: {
       type: 'Input',
-      name: 'in_topic'
+      name: 'in_topic',
+      width: 4
     },
     in_motion_message: {
       type: 'Input',
-      name: 'in_motion_message'
+      name: 'in_motion_message',
+      width: 4
     },
     in_no_motion_message: {
       type: 'Input',
-      name: 'in_no_motion_message'
-    },
-    name: {
-      type: 'Input',
-      name: 'name'
+      name: 'in_no_motion_message',
+      width: 4
     },
     out_motion_message: {
       type: 'Input',
-      name: 'out_motion_message'
+      name: 'out_motion_message',
+      width: 6
     },
     out_no_motion_message: {
       type: 'Input',
-      name: 'out_no_motion_message'
+      name: 'out_no_motion_message',
+      width: 6
+    },
+    name: {
+      type: 'Input',
+      name: 'name',
+      width: 4
     },
     min_delay: {
       type: 'Number',
-      name: 'min_delay'
+      name: 'min_delay',
+      width: 4
     },
     max_delay: {
       type: 'Number',
-      name: 'max_delay'
+      name: 'max_delay',
+      width: 4
     }
   }
 }
