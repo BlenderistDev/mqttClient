b2da2709e650b02f18b808426c5b0a892770e511
blenderist <demonx.blenderist@gmail.com>
2021-05-19 21:35:23 +0300

    Удалил неиспользуемый код из компонента layout для полей формбилдера

--- front/src/components/FormBuilder/fields/Layout.vue
+++ front/src/components/FormBuilder/fields/Layout.vue
@@ -9,35 +9,11 @@ div
 </template>
 
 <script>
-import { computed, toRefs } from "vue";
-import { useStore } from "vuex";
-import _ from "lodash";
-
 export default {
   props: {
-    type: {
-      default: "",
-    },
     field: {
       required: true,
     },
-    config: {
-      default: {},
-    },
-  },
-  setup(props) {
-    const store = useStore();
-    const { config, field } = toRefs(props);
-    return {
-      value: computed({
-        get: () => config.value[field.value.name],
-        set: (value) =>
-          store.dispatch(
-            "modules/updateConfig",
-            _.set(config.value, field.value.name, value)
-          ),
-      }),
-    };
   },
 };
 </script>
