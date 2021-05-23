80843f1a5844c954b0d11fb040c05cc549c54f0c
blenderist <demonx.blenderist@gmail.com>
2021-05-23 11:47:28 +0300

    Отключил логер для api сервера

--- src/Api/app.js
+++ src/Api/app.js
@@ -1,7 +1,5 @@
 import express from 'express';
-import path from 'path';
 import cookieParser from 'cookie-parser';
-import logger from 'morgan';
 import fs from 'fs';
 import {router} from './router.js';
 import cors from 'cors';
@@ -11,10 +9,6 @@ import { socketHandler } from './socket.js'
 
 const app = express();
 
-// cоздаем поток для логгера
-const accessLogStream = fs.createWriteStream(path.join(process.cwd(), 'log', 'access.log'), {flags: 'a'});
-// запускаем логгер
-app.use(logger('combined', {stream: accessLogStream}));
 // подключаем папку со статикой
 app.use(express.static('front/dist'));
 // обработка json
