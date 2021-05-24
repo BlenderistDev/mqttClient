4fbc35c09c7f683af625b198a65c577626170648
BlenderistDev <demonx.blenderist@gmail.com>
2021-05-24 14:11:48 +0300

    Debounce для отправки значений конфига

--- front/src/services/configValue.js
+++ front/src/services/configValue.js
@@ -6,6 +6,6 @@ export const getConfigValue = (config, field) => {
   const store = useStore();
   return computed({
     get: () => config.value[field.value.name],
-    set: value => store.dispatch("modules/updateConfig", _.set(config.value, field.value.name, value)),
+    set: _.debounce(value => store.dispatch("modules/updateConfig", _.set(config.value, field.value.name, value)), 1000),
   })
 }
