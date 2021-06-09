7ecec1dd7f0148cb883baf015bfb5ac5d7e47cb6
blenderist <demonx.blenderist@gmail.com>
2021-06-09 20:38:59 +0300

    Фильтрация для списка топиков

--- front/src/components/Messages/Topic/TopicMessages.vue
+++ front/src/components/Messages/Topic/TopicMessages.vue
@@ -1,12 +1,16 @@
 <template lang="pug">
-Topic(
-  v-for="(topicMessages, topic) in messages"
-  :messages="topicMessages"
-  :topic="topic"
-)
+div
+  input.filter(v-model="topicFilter")
+  template(v-for="(topicMessages, topic) in messages")
+    Topic(
+      v-if="topic.includes(topicFilter)"
+      :messages="topicMessages"
+      :topic="topic"
+    )
 </template>
 
 <script>
+import { ref } from "vue";
 import { useStore } from "vuex";
 import Topic from "./Topic";
 
@@ -16,9 +20,17 @@ export default {
   },
   setup() {
     const store = useStore();
+    const topicFilter = ref("");
     return {
+      topicFilter,
       messages: store.state.messages.groupedMessages,
     };
   },
 };
 </script>
+
+<style scoped>
+.filter {
+  width: 100%;
+}
+</style>
