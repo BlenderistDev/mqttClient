ede23d894ad50052797f2faaa5c3742b756984a5
BlenderistDev <demonx.blenderist@gmail.com>
2021-06-10 12:39:08 +0300

    Возможность фильтрации по сообщению на списке всех сообщений

--- front/src/components/Messages/AllMessages/AllMessages.vue
+++ front/src/components/Messages/AllMessages/AllMessages.vue
@@ -7,6 +7,10 @@ div
     .col-md-1 On page:
     .col-md-1
       input(v-model='prePage')
+  .row
+    .col-md-1 Message:
+    .col-9
+      input(v-model='messageFilter')
   template(v-for="(message, key) in filtredMessages")
     .card(v-if="key < countToShow")
       .card-header {{ message.topic }}
@@ -30,10 +34,12 @@ export default {
     const store = useStore();
     const messages = computed(() => store.state.messages.messages);
     const filterTopic = ref("");
+    const messageFilter = ref("");
     const filtredMessages = computed(() =>
       _.filter(
         messages.value,
-        (message) => !filterTopic.value || message.topic.includes(filterTopic.value)
+        (message) => (!filterTopic.value || message.topic.includes(filterTopic.value))
+          && (!messageFilter.value || message.message.includes(messageFilter.value))
       )
     );
     const page = ref(1);
@@ -41,6 +47,7 @@ export default {
     return {
       messages,
       filterTopic,
+      messageFilter,
       filtredMessages,
       page,
       prePage,
