ab9579cb7e775ab4d777ec46f69c2adaa2a67d0f
blenderist <demonx.blenderist@gmail.com>
2021-06-13 16:48:03 +0300

    Рефакторинг вывода всех сообщений. Вынес пагинацию и вывод списка собщений в отдельный компонент

--- front/src/components/Messages/AllMessages/AllMessages.vue
+++ front/src/components/Messages/AllMessages/AllMessages.vue
@@ -1,41 +1,23 @@
 <template lang="pug">
-div
-  template(v-for="(message, key) in filterMessages")
-    .card(v-if="key < countToShow")
-      .card-header {{ message.topic }}
-      .card-body.justify-content-between
-        Message(:message="message")
-  .btn.btn-secondary.d-flex.justify-content-center(v-if="filterMessages.length > countToShow && page > 1" @click="page--") Less
-  .btn.btn-secondary.d-flex.justify-content-center(v-if="filterMessages.length > countToShow" @click="page++") More
+MessageList(:messages="filterMessages")
+  template(v-slot:default="slotProps")
+    .card-header {{ slotProps.message.topic }}
 </template>
 
 <script>
-import { ref, computed } from "vue";
+import { computed } from "vue";
 import { useStore } from "vuex";
-import Message from "../Message";
-import Controls from "../Controls";
+import MessageList from "../MessageList";
 
 export default {
   components: {
-    Message,
-    Controls
+    MessageList,
   },
   setup() {
     const store = useStore();
-    const page = ref(1);
-    const perPage = computed(() => store.state.messages.perPage);
     return {
       filterMessages: computed(() => store.getters["messages/filterMessages"]),
-      page,
-      perPage,
-      countToShow: computed(() => page.value * perPage.value),
     };
   },
 };
 </script>
-
-<style scoped>
-.btn {
-  margin-top: 20px;
-}
-</style>
---
+++ front/src/components/Messages/MessageList.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+div
+  template(v-for="(message, key) in messages")
+    .card(v-if="key < countToShow")
+      slot(:message="message")
+      .card-body.justify-content-between
+        Message(:message="message")
+  .btn.btn-secondary.d-flex.justify-content-center(v-if="messages.length > countToShow && page > 1" @click="page--") Less
+  .btn.btn-secondary.d-flex.justify-content-center(v-if="messages.length > countToShow" @click="page++") More
+</template>
+
+<script>
+import { ref, computed } from "vue";
+import Message from "./Message";
+import Controls from "./Controls";
+import { useStore } from "vuex";
+
+export default {
+  name: "MessageList",
+  components: {
+    Message,
+    Controls,
+  },
+  props: {
+    messages: Array,
+  },
+  setup() {
+    const store = useStore();
+    const page = ref(1);
+    const perPage = computed(() => store.state.messages.perPage);
+    return {
+      page,
+      perPage,
+      countToShow: computed(() => page.value * perPage.value),
+    };
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  margin-top: 20px;
+}
+</style>
