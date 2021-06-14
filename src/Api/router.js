2e0338142cf2170882ea7954e1fac9966724e3bc
blenderist <demonx.blenderist@gmail.com>
2021-06-14 16:24:42 +0300

    Приведение имени модуля к строке в роутере

--- src/Api/router.js
+++ src/Api/router.js
@@ -7,10 +7,10 @@ const router = express.Router();
 
 router.get('/api/module/list', async (req, res,) =>res.send(await fs.promises.readdir('src/Modules').then(module => module)));
 
-router.get('/api/module/:moduleName', async (req, res) => res.send(await getModuleConfig(req.params.moduleName)));
+router.get('/api/module/:moduleName', async (req, res) => res.send(await getModuleConfig(req.params.moduleName.toString())));
 
 router.post('/api/module/:moduleName', async (req, res) => res.send(await setModuleConfig(req.body.config)));
 
-router.get('/api/restart/:moduleName', (req, res) => res.send(restartModule(req.params.moduleName)));
+router.get('/api/restart/:moduleName', (req, res) => res.send(restartModule(req.params.moduleName.toString())));
 
 export { router };
