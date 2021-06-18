bf609ae7d835f60dd82739961eed657233044399
blenderist <demonx.blenderist@gmail.com>
2021-06-18 20:16:21 +0300

    Изменение внешнего вида настроек модуля сохранения в mysql

--- src/Modules/DatabaseStorage/config.js
+++ src/Modules/DatabaseStorage/config.js
@@ -5,23 +5,31 @@ export default {
     host: {
       type: 'Input',
       name: 'host',
-      validator: ['required']
+      validator: ['required'],
+      width: 4,
+      placeholder: '127.0.0.1',
     },
     user: {
       type: 'Input',
       name: 'user',
+      width: 4
     },
     password: {
-      type: 'Input',
+      type: 'Password',
       name: 'password',
+      width: 4
     },
     database: {
       type: 'Input',
       name: 'database',
+      validator: ['required'],
+      width: 4
     },
     table: {
       type: 'Input',
       name: 'table',
+      validator: ['required'],
+      width: 4,
     },
   }
 }
