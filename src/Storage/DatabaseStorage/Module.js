40f0cb954ba9e130cc22c2f15d1855a3447e64c8
BlenderistDev <demonx.blenderist@gmail.com>
2021-07-08 13:32:13 +0300

    Перевел хранилище в базе данных на Sequelize

--- src/Storage/DatabaseStorage/Module.js
+++ src/Storage/DatabaseStorage/Module.js
@@ -1,27 +1,24 @@
 import { mqttClient } from '../../Components/SocketClient.js'
 import { config } from "../../Components/ModuleConfig.js";
-import mysql from 'mysql';
-import _ from 'lodash'
+import Sequelize from 'sequelize';
 
-const connection = mysql.createConnection({
+const sequelize = new Sequelize(config.database, config.user, config.password, {
   host: config.host,
-  user: config.user,
-  password: config.password
+  dialect: 'mysql',
+  logging:false
 });
 
-connection.connect();
+class Messages extends Sequelize.Model {}
 
-const table = connection.escapeId(config.table)
-const database = connection.escapeId(config.database)
+Messages.init({
+  topic: Sequelize.DataTypes.STRING,
+  message: Sequelize.DataTypes.STRING,
+  qos: Sequelize.DataTypes.INTEGER,
+  date: Sequelize.DataTypes.DATE
+}, { sequelize, modelName: config.table });
 
-connection.query(`USE ${database};`)
-connection.query(`CREATE TABLE IF NOT EXISTS ${table} (
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  topic VARCHAR(512) NOT NULL,
-  message TEXT NOT NULL,
-  qos INT(2),
-  date TIMESTAMP NOT NULL
-);`)
+
+sequelize.sync();
 
 let messages = []
 mqttClient.on('message', message => messages.push(message))
@@ -34,13 +31,6 @@ const flushMessages = () => {
 
 setInterval(() => {
   const messages = flushMessages()
-  const data = _.chain(messages)
-    .map(message => [message.topic, message.message, message.qos, new Date(message.date)])
-    .flatMap()
-    .value()
-
-  const valuesTemplate = _.repeat('(?, ?, ?, ?),', messages.length).slice(0, -1)
-  connection.query(`INSERT INTO ${table}(topic, message, qos, date) VALUES ${valuesTemplate}`, data, function(err, rows, fields) {
-    if (err) console.log(err);
-  })
+  Messages.bulkCreate(messages)
 }, 10000)
+
