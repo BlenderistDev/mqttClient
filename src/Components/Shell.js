456ea89754bc4aeb7b1fd51f9c45324e5ac0926c
BlenderistDev <demonx.blenderist@gmail.com>
2021-07-23 13:52:42 +0300

    Выполнение shell команд в модуле ShellSender только если пользователь не рут

--- src/Components/Shell.js
+++ src/Components/Shell.js
@@ -1,3 +1,16 @@
 import shell from 'shelljs'
+import { notificationClient } from "./SocketClient.js";
+import { name } from "./ModuleConfig.js";
 
-export const shellExec = command => shell.exec(command, {'silent': true}).toString()
+export const shellExec = command => shell.exec(command, {'silent': true}).toString().trim()
+
+export const execIfNotRoot = func => {
+  if (shellExec("whoami") === 'root') {
+    notificationClient.send({
+      module: name,
+      message: "Root user is forbidden"
+    })
+  } else {
+    func()
+  }
+}
--- src/Modules/Modules/ShellSender/Module.js
+++ src/Modules/Modules/ShellSender/Module.js
@@ -2,9 +2,10 @@ import { sendDiscoveryMessage } from '../../../Components/HomeAssistant.js'
 import md5 from 'md5'
 import { mqttClient } from '../../../Components/SocketClient.js'
 import { config, topic } from "../../../Components/ModuleConfig.js"
-import { shellExec } from '../../../Components/Shell.js'
+import { execIfNotRoot, shellExec } from '../../../Components/Shell.js'
 
-const moduleTopic = `${topic}/${config.topic}`
-sendDiscoveryMessage(md5(moduleTopic), moduleTopic, 'sensor', {})
-
-setInterval(() => mqttClient.send(moduleTopic, shellExec(config.command)), config.interval * 1000)
+execIfNotRoot(() => {
+  const moduleTopic = `${topic}/${config.topic}`
+  sendDiscoveryMessage(md5(moduleTopic), moduleTopic, 'sensor', {})
+  setInterval(() => mqttClient.send(moduleTopic, shellExec(config.command)), config.interval * 1000)
+})
