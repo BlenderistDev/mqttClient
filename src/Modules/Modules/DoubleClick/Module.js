8e9632f59ed33411593150f7757690a5d017e57c
BlenderistDev <demonx.blenderist@gmail.com>
2023-08-25 23:46:43 +0300

    double click name update

--- src/Modules/Modules/DoubleClick/Module.js
+++ src/Modules/Modules/DoubleClick/Module.js
@@ -1,9 +1,8 @@
-import md5 from 'md5'
 import {ClickWatcher} from './Click.js'
 import { mqttClient } from '../../../Components/SocketClient.js'
 import { config, topic } from "../../../Components/ModuleConfig.js";
 
-const clickWatcher = new ClickWatcher(config, topic + '/' + md5(config.in_topic))
+const clickWatcher = new ClickWatcher(config, topic + '/' + config.name)
 
 mqttClient.on('message', (mqttMessage) => {
   if (clickWatcher.getInTopic() === mqttMessage.topic) {
